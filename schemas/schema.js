b88bc466be87f9df4cf478dcb8b6469ca1dd3e77
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-10-29 21:01:14 +0100

    Remove unused gridrow

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,9 +1,8 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 import containerDoc from './containerDoc'
-import gridRow from './gridRow'
 import funkyTable from './funkyTable'
 
 export default createSchema({
   name: 'default',
-  types: [containerDoc, funkyTable, gridRow]
+  types: [containerDoc, funkyTable]
 })
